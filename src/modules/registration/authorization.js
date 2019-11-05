8a3bee4740afa1134ae7e4e894c0280a58053880
Roman Kolpachev <roman.kolpachev@yandex.ru>
2019-11-05 23:09:00 +0700

    fix authorization registration

--- src/modules/registration/authorization.js
+++ src/modules/registration/authorization.js
@@ -7,9 +7,8 @@ function checkAuthorized(event) {
     const name = form.elements.name;
     const email = form.elements.email;
     const password = form.elements.password;
-    const checkbox = form.elements.checkbox;
 
-    if (email.value === "roman@yandex.ru" && password.value === "123" && checkbox.value === "on") {
+    if (email.value === "roman@yandex.ru" && password.value === "123") {
         localStorage.setItem("authorized", `${name.value}`);
         localStorage.setItem("password", `${password.value}`);
         window.location.href = `/index.html`;
